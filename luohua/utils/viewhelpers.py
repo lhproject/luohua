f582f245f8a0a127e60cc803e0db6c5127266e53
Wang Xuerui <idontknw.wang@gmail.com>
2013-07-21 17:39:27 +0800

    utils.viewhelpers: 让 parse_form 支持参数默认值

--- luohua/utils/viewhelpers.py
+++ luohua/utils/viewhelpers.py
@@ -29,11 +29,18 @@ def jsonreply(**kwargs):
     return 200, kwargs, {}
 
 
-def parse_form(request, *args):
-    '''从请求对象顺序解出表单参数成 tuple.'''
+def parse_form(request, *args, **kwargs):
+    '''从请求对象顺序解出表单参数成 tuple.
+
+    支持将一些参数设置为可选, 这些参数的默认值用同名的命名参数方式传入.
+
+    '''
 
     frm = request.form
-    return tuple(frm[i] for i in args)
+    return tuple(
+            frm[i] if i not in kwargs else frm.get(i, kwargs[i])
+            for i in args
+            )
 
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
