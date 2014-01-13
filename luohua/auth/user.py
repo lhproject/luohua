bc5b6f1651a9c45f4ade9d9068b6a510152842c2
Wang Xuerui <idontknw.wang@gmail.com>
2014-01-13 12:11:20 +0800

    auth.user: 利用数据库抽象层

--- luohua/auth/user.py
+++ luohua/auth/user.py
@@ -22,8 +22,8 @@ from __future__ import unicode_literals, division
 import six
 
 from weiyu.db.mapper import mapper_hub
-from weiyu.db.mapper.base import Document
 
+from ..utils.dblayer import RiakDocument
 from ..utils.stringop import escape_lucene
 from .passwd import Password
 
@@ -31,24 +31,11 @@ USER_STRUCT_ID = 'luohua.user'
 mapper_hub.register_struct(USER_STRUCT_ID)
 
 
-class User(Document):
+class User(RiakDocument):
     struct_id = USER_STRUCT_ID
 
     def __init__(self, data=None, uid=None, rawobj=None):
-        super(User, self).__init__()
-
-        if data is not None:
-            # 使用解码后的形式作为数据
-            # 这里调用 dict 的构造函数在 CPython 下有 overhead, 不过在 profile
-            # 之前还是算了吧, 不太值得优化
-            # 虽说留着没什么问题... 但看上去很不优雅. 那么换用更批量一点的形式
-            # super(User, self).__init__(**self.decode(data))
-            self.update(self.decode(data))
-
-        if uid is not None:
-            self['id'] = uid
-
-        self._rawobj = rawobj
+        super(User, self).__init__(data, uid, rawobj)
 
     def chkpasswd(self, psw):
         '''进行密码验证。'''
@@ -57,64 +44,21 @@ class User(Document):
         return psw_obj.check(psw)
 
     @classmethod
-    def _from_obj(cls, obj):
-        return cls(obj.data, obj.key, obj) if obj.exists else None
-
-    @classmethod
-    def find(cls, uid):
-        '''按文档 ID 获取一个用户.'''
-
-        with cls.storage as conn:
-            obj = conn.get(uid)
-            return cls._from_obj(obj)
-
-    @classmethod
     def find_by_name(cls, name):
         '''寻找登陆身份符合 name 的用户。'''
 
         name_escaped = escape_lucene(name)
-        with cls.storage as conn:
-            # 这里假设存放用户数据的 bucket 上已经启用了 Riak Search
-            # 并且定义了项目自带的 schema. 不启用 schema 在这里虽说没什么问题
-            # 但也许会导致返回结果多出几个用不着的字段 (我没试),
-            # 所以最好还是加上.
-            #
-            # Riak Search 的相关页面
-            # http://docs.basho.com/riak/latest/dev/advanced/search/
-            # 和 Search Schema 的内容
-            # TODO: 根据 name 的形式 (含 '@', 全数字等) 在这里就定下查询字段
-            query_expr = 'a:"%s" e:"%s"' % (name_escaped, name_escaped, )
-
-            # protobuf 协议要求查询字串必须是字节流, 所以编个码
-            # HTTP 协议无所谓, unicode 或者 bytes 都可以
-            result = conn.search(query_expr.encode('utf-8'))
-
-            # 检查下结果的数量, 应该只有一个. 如果不是一个就抛异常
-            num, docs = result['num_found'], result['docs']
-            if num == 0:
-                raise KeyError(name_escaped)
-            elif num > 1:
-                raise ValueError(
-                        'Found >1 user with login identity %s: %s' % (
-                            repr(name_escaped),
-                            repr(docs),
-                            )
-                        )
-
-            # 拿出 ID, 生成对象返回
-            uid = docs[0]['id']
-            obj = conn.get(uid)
-            return cls._from_obj(obj)
-
-    def save(self):
-        with self.storage as conn:
-            obj = self._rawobj if self._rawobj is not None else conn.new()
-            obj.key, obj.data = self.get('id'), self.encode()
-
-            obj.store()
-
-            # 刷新对象关联信息
-            self['id'], self._rawobj = obj.key, obj
+        # 这里假设存放用户数据的 bucket 上已经启用了 Riak Search
+        # 并且定义了项目自带的 schema. 不启用 schema 在这里虽说没什么问题
+        # 但也许会导致返回结果多出几个用不着的字段 (我没试),
+        # 所以最好还是加上.
+        #
+        # Riak Search 的相关页面
+        # http://docs.basho.com/riak/latest/dev/advanced/search/
+        # 和 Search Schema 的内容
+        # TODO: 根据 name 的形式 (含 '@', 全数字等) 在这里就定下查询字段
+        query_expr = 'a:"%s" e:"%s"' % (name_escaped, name_escaped, )
+        return cls.fetch_fts(query_expr)
 
 
 # 数据库序列化/反序列化
