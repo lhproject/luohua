fa7cdd9f03258bafed8288f9a16095ec570da0a0
Wang Xuerui <idontknw.wang@gmail.com>
2014-01-13 12:13:42 +0800

    auth.role: 利用数据库抽象层; 统一查询方式为通过文档 ID

--- luohua/auth/role.py
+++ luohua/auth/role.py
@@ -20,24 +20,16 @@
 from __future__ import unicode_literals, division
 
 from weiyu.db.mapper import mapper_hub
-from weiyu.db.mapper.base import Document
 
+from ..utils.dblayer import RiakDocument
 from ..utils.stringop import escape_lucene
 
 ROLE_STRUCT_ID = 'luohua.role'
 mapper_hub.register_struct(ROLE_STRUCT_ID)
 
 
-class Role(Document):
+class Role(RiakDocument):
     struct_id = ROLE_STRUCT_ID
-
-    def __init__(self, data):
-        # 使用解码后的形式作为数据
-        # 这里调用 dict 的构造函数在 CPython 下有 overhead, 不过在 profile
-        # 之前还是算了吧, 不太值得优化
-        # 虽说留着没什么问题... 但看上去很不优雅. 那么换用更批量一点的形式
-        # super(User, self).__init__(**self.decode(data))
-        self.update(self.decode(data))
 
     def __contains__(self, key):
         return self.hascap(key)
@@ -48,37 +40,11 @@ class Role(Document):
         return cap in self['caps']
 
     @classmethod
-    def find(cls, name):
-        '''寻找名为 name 的角色。'''
-
-        name_escaped = escape_lucene(name)
-        with cls.storage as conn:
-            r = conn.search(('n:"%s"' % (name_escaped, )).encode('utf-8'))
-
-            num, docs = r['num_found'], r['docs']
-            if num == 0:
-                raise KeyError(name_escaped)
-            elif num > 1:
-                raise ValueError(
-                        'Found >1 role named %s: %s' % (
-                            repr(name_escaped),
-                            repr(docs),
-                            )
-                        )
-
-            # 拿出 ID, 生成对象返回
-            rid = docs[0]['id']
-            data = conn.get(rid).data
-            data['rid'] = rid
-            return cls(data)
-
-    @classmethod
-    def allcaps(cls, *roles):
+    def allcaps(cls, *rids):
         '''返回 ``roles`` 列表所示角色拥有的所有能力.'''
 
         result = set()
-        for role_name in roles:
-            role = cls.find(role_name)
+        for role in cls.fetch_multiple(rids):
             result.update(role['caps'])
 
         return result
@@ -87,10 +53,7 @@ class Role(Document):
 # 数据库序列化/反序列化
 @mapper_hub.decoder_for(ROLE_STRUCT_ID, 1)
 def role_dec_v1(data):
-    rid = data.get('rid', None)
-
     return {
-            'rid': rid,
             'name': data['n'],
             'caps': data['c'].split(' '),
             }
