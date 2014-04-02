b21c9c42aa1a188f72ec1059459a599be705c8ab
Wang Xuerui <idontknw.wang@gmail.com>
2014-04-02 20:43:11 +0800

    API v1: account: ident-query: 初始实现

--- luohua/app/v1/account.py
+++ luohua/app/v1/account.py
@@ -19,10 +19,19 @@
 
 from __future__ import unicode_literals, division
 
-from weiyu.shortcuts import *
+from weiyu.shortcuts import http, jsonview
 from weiyu.utils.decorators import only_methods
 
 from ...utils.viewhelpers import jsonreply, parse_form
+
+from ...auth import ident
+
+IDENT_CHECK_RETCODE_MAP = {
+        ident.IDENT_OK: (True, ident.IDENT_OK, ),
+        ident.CHECK_IDENT_NOTFOUND: (False, 2, ),
+        ident.CHECK_IDENT_INVALID_INPUT: (False, 257, ),
+        ident.CHECK_IDENT_WRONG: (False, 13, ),
+        }
 
 
 @http
@@ -47,6 +56,107 @@ def account_fcntl_v1_view(request, userid):
 @jsonview
 def account_unlink_v1_view(request, userid):
     raise NotImplementedError
+
+
+@http
+@jsonview
+@only_methods(['POST', ])
+def ident_query_v1_view(request):
+    '''v1 实名信息查询接口.
+
+    :Allow: POST
+    :URL 格式: ``accounts/ident/query/``
+    :POST 参数:
+        ======== ======== =================================================
+         字段     类型     说明
+        ======== ======== =================================================
+         number   unicode  **必须** 学号/工号
+         idtype   int      **必须** 身份信息类型, 请参见实名信息模块文档
+         idnum    unicode  **必须** 身份信息
+        ======== ======== =================================================
+
+    :返回:
+        :r:
+            ===== =========================================================
+             0     验证通过
+             2     给定的学号/工号不存在
+             13    身份信息不匹配
+             17    给定的学号/工号已有对应帐号
+             22    传入参数格式不正确
+             257   身份信息格式不正确
+            ===== =========================================================
+
+        若验证通过, 将返回所查询记录的基本信息:
+
+            ==== ======== =================================================
+            属性  类型     说明
+            ==== ======== =================================================
+             n    unicode  所查询个体的真实姓名.
+             g    int      所查询个体的性别; 含义请参见实名信息模块文档.
+             t    int      本条实名信息记录的类型.
+            ==== ======== =================================================
+
+        若所查询个体身份为学生, 返回还将包含一些学生特定的信息:
+
+            ==== ======== =================================================
+            属性  类型     说明
+            ==== ======== =================================================
+             ss   unicode  所属学院全称, 如 ``'数字媒体学院'``.
+             sm   unicode  所属专业代码, 如 ``'0305'``.
+             sc   int      所属班级序号, 从 1 开始.
+             sy   int      入学年份.
+            ==== ======== =================================================
+
+    :副作用: 无
+
+    '''
+
+    try:
+        number, idtype, idnum = parse_form(
+                request,
+                'number',
+                'idtype',
+                'idnum',
+                )
+    except KeyError:
+        return jsonreply(r=22)
+
+    try:
+        idtype = int(idtype)
+    except ValueError:
+        return jsonreply(r=22)
+
+    try:
+        number = number.decode('utf-8')
+        idnum = idnum.decode('utf-8')
+    except ValueError:
+        return jsonreply(r=22)
+
+    # 做检查
+    chkresult = ident.FrozenIdent.check_ident(number, idtype, idnum)
+    success, retcode = IDENT_CHECK_RETCODE_MAP[chkresult]
+    if not success:
+        return jsonreply(r=retcode)
+
+    # 检查通过, 取记录, 返回信息
+    fident = ident.FrozenIdent.fetch(number)
+    typ = fident['type']
+    result = {
+            'r': 0,
+            'n': fident['name'],
+            'g': fident['gender'],
+            't': typ,
+            }
+
+    if typ in ident.IDENT_TYPES_CURRENT_STUDENT:
+        result.update({
+                'ss': fident['student_school'],
+                'sm': fident['student_major'],
+                'sc': fident['student_class'],
+                'sy': fident['student_year'],
+                })
+
+    return jsonreply(**result)
 
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
