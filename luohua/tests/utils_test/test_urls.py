fecba1ba4055ceb01f7c2668eb6193e2524baa30
Wang Xuerui <idontknw.wang@gmail.com>
2014-04-03 19:46:29 +0800

    utils: 添加 URL 处理组件

---
+++ luohua/tests/utils_test/test_urls.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 落花 / 测试套件 / 工具 / URL 处理
+#
+# Copyright (C) 2013-2014 JNRain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+from nose.tools import assert_raises
+
+from ..utils import Case
+
+from weiyu import registry
+from luohua.utils import urls
+
+
+class TestURLs(Case):
+    @classmethod
+    def setup_class(cls):
+        cls._old_bridge_cfg = registry._registries['luohua.bridge']
+        registry._registries['luohua.bridge'] = {
+                'api_domain': 'api.example.com',
+                'frontend_domain': 'example.com',
+                }
+
+    @classmethod
+    def teardown_class(cls):
+        registry._registries['luohua.bridge'] = cls._old_bridge_cfg
+
+    def test_get_api_domain(self):
+        assert urls.get_api_domain() == 'api.example.com'
+
+    def test_get_frontend_domain(self):
+        assert urls.get_frontend_domain() == 'example.com'
+
+    def test_get_https_url(self):
+        url1 = urls.get_https_url('abc.com', 'a/b', '', '')
+        url2 = urls.get_https_url('abc.com', '/a/b', '', '')
+        url3 = urls.get_https_url('abc.com', 'a/b/', '', '')
+        url4 = urls.get_https_url('abc.com', 'a/b/', 'abc=123', '')
+        url5 = urls.get_https_url('abc.com', 'a/b/', '', '!/foo')
+        url6 = urls.get_https_url('abc.com', 'a/b/', 'abc=123', '!/foo')
+
+        assert url1 == 'https://abc.com/a/b'
+        assert url2 == 'https://abc.com/a/b'
+        assert url3 == 'https://abc.com/a/b/'
+        assert url4 == 'https://abc.com/a/b/?abc=123'
+        assert url5 == 'https://abc.com/a/b/#!/foo'
+        assert url6 == 'https://abc.com/a/b/?abc=123#!/foo'
+
+    def test_get_api_url(self):
+        assert urls.get_api_url('a/b') == 'https://api.example.com/a/b'
+        assert urls.get_api_url('/a/b') == 'https://api.example.com/a/b'
+        assert urls.get_api_url('a/b/') == 'https://api.example.com/a/b/'
+
+    def test_get_frontend_url(self):
+        assert urls.get_frontend_url('a/b') == 'https://example.com/a/b'
+        assert urls.get_frontend_url('/a/b') == 'https://example.com/a/b'
+        assert urls.get_frontend_url('a/b/') == 'https://example.com/a/b/'
+
+    def test_reverse_api_url(self):
+        url1 = urls.reverse_api_url('api:session-ping-v1')
+        assert url1 == 'https://api.example.com/v1/session/ping/'
+
+    def test_reverse_api_url_params(self):
+        url2 = urls.reverse_api_url(
+                'api:ident-activate-v1',
+                activation_key='123',
+                )
+        assert url2 == 'https://api.example.com/v1/account/ident/activate/123/'
+
+    def test_reverse_api_url_exc_behavior(self):
+        assert_raises(ValueError, urls.reverse_api_url, 'non-existent-ep')
+        assert_raises(ValueError, urls.reverse_api_url, 'api:non-existent-ep')
+        assert_raises(ValueError, urls.reverse_api_url, 'xyz:non-existent-ep')
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
---
+++ luohua/utils/urls.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 落花 / 工具 / URL 处理
+#
+# Copyright (C) 2013-2014 JNRain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+__all__ = [
+        'get_api_domain',
+        'get_frontend_domain',
+        'get_https_url',
+        'get_api_url',
+        'get_frontend_url',
+        'reverse_api_url',
+        ]
+
+import six
+urlparse = six.moves.urllib.parse
+
+from weiyu import registry
+from weiyu.shortcuts import reverse_http
+
+BRIDGE_CONF_REGISTRY = 'luohua.bridge'
+
+
+def get_api_domain():
+    return registry.request(BRIDGE_CONF_REGISTRY)['api_domain']
+
+
+def get_frontend_domain():
+    return registry.request(BRIDGE_CONF_REGISTRY)['frontend_domain']
+
+
+def get_https_url(netloc, path, query, fragment):
+    return urlparse.urlunsplit(('https', netloc, path, query, fragment, ))
+
+
+def get_api_url(path):
+    return get_https_url(get_api_domain(), path, '', '')
+
+
+def get_frontend_url(path):
+    return get_https_url(get_frontend_domain(), path, '', '')
+
+
+def reverse_api_url(endpoint, **kwargs):
+    path = reverse_http(endpoint, **kwargs)
+    return get_api_url(path)
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
