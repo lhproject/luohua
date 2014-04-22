afd15f313d5b0daca1a1a5bc67e779253fb2c4fd
Wang Xuerui <idontknw.wang@gmail.com>
2014-04-22 15:43:34 +0800

    rtserver: 默认开启实时服务器的 SSL 支持

--- rtserver.py
+++ rtserver.py
@@ -18,14 +18,26 @@ def main():
     socketio_conf = rt_conf['socketio']
     listen_conf = socketio_conf['listen']
     policy_conf = socketio_conf['policy_server']
+    ssl_conf = listen_conf.get('ssl', {})
 
-    server.cli_server(
-            'socketio',
-            listen=(listen_conf['host'], listen_conf['port']),
-            resource='socket.io',
-            policy_server=policy_conf['enabled'],
-            policy_listener=(policy_conf['host'], policy_conf['port']),
-            )
+    options = {
+            'listen': (listen_conf['host'], listen_conf['port']),
+            'resource': 'socket.io',
+            'policy_server': policy_conf['enabled'],
+            'policy_listener': (policy_conf['host'], policy_conf['port']),
+            }
+
+    if ssl_conf.get('enabled', True):
+        # ssl_args
+        # 就算根本没设置 SSL 选项也默认成 True, 这里没写错, 就是逼着大家去部署
+        # SSL 的. 要想禁用 SSL 的话至少要看到这里和 Rain.d/lh.rt.example.yml
+        # 的其中之一, 我们的目的也就达到了.
+        options.update({
+                'keyfile': ssl_conf['keyfile'],
+                'certfile': ssl_conf['certfile'],
+                })
+
+    server.cli_server('socketio', **options)
 
 
 if __name__ == '__main__':
