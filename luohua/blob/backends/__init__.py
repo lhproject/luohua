a49710797e1524f52294b843f8849a8571953e90
Wang Xuerui <idontknw.wang@gmail.com>
2014-08-20 01:27:23 +0800

    blob: WIP: Riak 后端不完整实现

---
+++ luohua/blob/backends/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 落花 / Blob 公共模块 / 存储后端 / 包
+#
+# Copyright (C) 2013-2014 JNRain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+
+class BlobBackend(object):
+    def put(self, data, **kwargs):
+        # 存储一个新的 Blob 对象, 返回其 Blob ID
+        raise NotImplementedError
+
+    def meta(self, blob_id, **kwargs):
+        # 获取 Blob 对象元数据
+        raise NotImplementedError
+
+    def get(self, blob_id, **kwargs):
+        # 读取 Blob 对象
+        raise NotImplementedError
+
+    def multipart_init(self, **kwargs):
+        # 初始化一个 multipart blob 上传会话, 返回一个上传 ID
+        raise NotImplementedError
+
+    def multipart_store(self, upload_id, part_seq, data, **kwargs):
+        # 向一个 multipart blob 上传会话添加 part
+        raise NotImplementedError
+
+    def multipart_finish(self, upload_id, parts, **kwargs):
+        # 完成一个 multipart blob 的上传, 返回 blob id
+        raise NotImplementedError
+
+    def multipart_abort(self, upload_id):
+        # 终止一个 multipart blob 的上传, 销毁所有已上传的 part
+        raise NotImplementedError
+
+    def multipart_list(self, upload_id):
+        # 获取一个 multipart blob 的 part 上传状态情况
+        raise NotImplementedError
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
---
+++ luohua/blob/backends/riaknative/blob.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 落花 / Blob 公共模块 / 存储后端 / 原生 Riak / Blob 对象
+#
+# Copyright (C) 2013-2014 JNRain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals, division
+
+from weiyu.db import db_hub
+from weiyu.db.mapper.base import Document
+from weiyu.helpers.metaprogramming import classproperty
+from weiyu.helpers.misc import smartbytes
+
+from ....auth import user
+from ....utils import sequences
+
+BLOB_STRUCT_ID = 'luohua.blob.riaknative'
+BLOB_METADATA_STRUCT_ID = 'luohua.blob.riaknative._meta'
+
+BLOB_CTIME_IDX = 'ctime_int'
+BLOB_OWNER_IDX = 'owner_bin'
+BLOB_IS_PARTIAL_IDX = 'partial_int'
+
+BLOB_FLAG_PARTIAL = 0x01
+BLOB_FLAG_MULTIPART = 0x02
+
+
+def make_blob_flags_int(partial, multipart):
+    '''生成 blob flag 整数.'''
+
+    flags = 0
+    if partial:
+        flags |= BLOB_FLAG_PARTIAL
+    if multipart:
+        flags |= BLOB_FLAG_MULTIPART
+
+    return flags
+
+
+def parse_blob_flags_int(flags):
+    '''解析 blob flag 整数.'''
+
+    return {
+            'partial': flags & BLOB_FLAG_PARTIAL != 0,
+            'multipart': flags & BLOB_FLAG_MULTIPART != 0,
+            }
+
+
+class RiakBlobMetadata(Document):
+    '''Blob 元数据.
+
+    因为该数据并非作为底层 Riak 对象的本体存在, 而是以元数据形式附着在实际
+    blob 数据上, 所以本类并不直接参与数据库操作, 而没有从 ``RiakDocument``
+    继承.
+
+    '''
+
+    struct_id = BLOB_METADATA_STRUCT_ID
+
+    @classmethod
+    def decode_from_obj(cls, obj, version=None):
+        '''从一个表示 blob 的 Riak 对象中解出元数据.'''
+
+        # 处理 _V 字段
+        # 这里的 copy 纯粹是为了不造成副作用的强迫症, 因为取出来的 RiakObject
+        # 不是在销毁前再也不会写入, 就是写入前马上被更新成正确的 usermeta
+        meta_dict = obj.usermeta.copy()
+        meta_dict[b'_V'] = int(meta_dict[b'_V'])
+
+        return cls.decode(meta_dict, version)
+
+    @classmethod
+    def encode_for_blob(cls, blob, version=None):
+        '''为一个 blob 对象编码元数据使其能够直接入库.'''
+
+        ret = cls.encode(blob, version)
+
+        # 处理 _V 字段
+        version = ret['_V']
+        del ret['_V']
+        ret[b'_V'] = smartbytes(str(version))
+
+        return ret
+
+
+@RiakBlobMetadata.decoder(1)
+def blob_metadata_dec_v1(obj):
+    flags = int(obj['flags'].decode('utf-8'))
+    parsed_flags = parse_blob_flags_int(flags)
+
+    result = {
+        'filename': obj['filename'].decode('utf-8'),
+        'ctime': int(obj['ctime'].decode('utf-8')) / 1000000.0,
+        'uid': obj['uid'].decode('utf-8'),
+    }
+
+    result.update(parsed_flags)
+    return result
+
+
+@RiakBlobMetadata.encoder(1)
+def blob_metadata_enc_v1(blob):
+    flags = make_blob_flags_int(blob.is_partial, blob.is_multipart)
+
+    return {
+            b'filename': smartbytes(blob.filename),
+            b'ctime': smartbytes(str(int(blob.ctime * 1000000))),
+            b'uid': smartbytes(blob.owner_uid),
+            b'flags': smartbytes(str(flags)),
+            }
+
+
+def new_blob_id():
+    '''新建一个 Blob ID.'''
+
+    return 'bb' + sequences.time_descending_short_suffixed()
+
+
+class RiakBlob(object):
+    '''使用 Riak 后端存储的 blob.'''
+
+    struct_id = BLOB_STRUCT_ID
+
+    def __init__(
+            self,
+            blob_id=None,
+            data=None,
+            filename=None,
+            content_type=None,
+            ctime=None,
+            owner_uid=None,
+            is_partial=False,
+            is_multipart=False,
+            ):
+        self._owner = None
+
+        if blob_id is None:
+            blob_id_to_use = new_blob_id()
+            self._rawobj = self.storage.new(
+                    key=blob_id_to_use,
+                    data=data,
+                    content_type=content_type,
+                    )
+
+            self._blob_id = blob_id_to_use
+            self.filename = filename
+            self.ctime = ctime
+            self._owner_uid = owner_uid
+            self.is_partial = is_partial
+            self.is_multipart = is_multipart
+        else:
+            obj = self._rawobj = self.storage.get(blob_id)
+            metadata = RiakBlobMetadata.decode_from_obj(obj)
+
+            self._blob_id = blob_id
+            self.filename = metadata['filename']
+            self.ctime = metadata['ctime']
+            self._owner_uid = metadata['uid']
+            self.is_partial = metadata['partial']
+            self.is_multipart = metadata['multipart']
+
+    @classproperty
+    def storage(cls):
+        '''``Document``-like interface for accessing underlying database.'''
+
+        return db_hub.get_storage(cls.struct_id)
+
+    @property
+    def blob_id(self):
+        return self._blob_id
+
+    @property
+    def data(self):
+        return self._rawobj.encoded_data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, bytes):
+            raise TypeError('blob data must be bytes')
+        self._rawobj.encoded_data = value
+
+    @property
+    def content_type(self):
+        return self._rawobj.content_type
+
+    @content_type.setter
+    def content_type(self, value):
+        self._rawobj.content_type = value
+
+    @property
+    def owner(self):
+        if self._owner is not None:
+            return self._owner
+
+        owner = self._owner = user.User.fetch(self._owner_uid)
+        return owner
+
+    @property
+    def owner_uid(self):
+        return self._owner_uid
+
+    @owner_uid.setter
+    def owner_uid(self, value):
+        # 清空可能缓存的上一个用户对象
+        self._owner_uid, self._owner = value, None
+
+    def get_encoded_metadata(self):
+        return RiakBlobMetadata.encode_for_blob(self)
+
+    def save(self):
+        encoded_metadata = self.get_encoded_metadata()
+        self._rawobj.usermeta = encoded_metadata
+
+        self._rawobj.store()
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
